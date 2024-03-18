ec142da47c7e48e772d87629b6e4950a673082e1
youngniiiiii <hanuje_@naver.com>
2024-03-18 12:44:04 +0900

    feat : port-4추가

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,6 +2,7 @@
 import project1 from '@/assets/images/project-front/port-1.webp';
 import project2 from '@/assets/images/project-front/port-2.webp';
 import project3 from '@/assets/images/project-front/port-3.webp';
+import project4 from '@/assets/images/project-front/port-4.webp';
 
 export const projectsData = [
   {
@@ -38,6 +39,18 @@ export const projectsData = [
     description: '- mixin 구성과 장바구니 기능 개발',
     technologies: 'SASS, Tailwind, html, Javascript, json-server',
     github: 'https://github.com/youngniiiiii/sayho-karly',
+    alt: '프로젝트',
+  },
+  {
+    id: 3,
+    title: 'YOUNGNI PORTFOLIO',
+    img: project4,
+    href: 'https://portfolio-youngni.vercel.app/',
+    intro: '반응형 디자인(모바일,태블릿,데스크탑), 다크모드 ',
+    details: '클론코딩 후 css수정, 애니메이션 추가',
+    description: '-',
+    technologies: 'Tailwind, html, Javascript, framer-motion',
+    github: 'https://github.com/youngniiiiii/',
     alt: '프로젝트',
   },
 ];
